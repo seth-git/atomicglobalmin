0f9c9d0401d030218ee9fcb6a9517ac5fdbb9c51
sethcall <sethcall@39e8ab93-2646-4851-8b6d-0fe033186267>
2011-03-05 15:36:01 +0000

    Bug fixes for random number generation.  Also, improved the LJ7 examples

--- trunk/unit.cc
+++ trunk/unit.cc
@@ -47,23 +47,23 @@ bool ccLibReadTest()
 	cout << "Reading the energy only from file: " << unitTestFile.c_str() << endl;
 	Energy::readOutputFileWithCCLib(unitTestFile.c_str(), energy, NULL, cclibInstalled, openedFile, readEnergy, obtainedGeometry);
 	if (!cclibInstalled) {
-		cout << "Testing of cclib Failed!" << endl;
+		cout << "Testing of cclib failed!" << endl;
 		cout << "\tReason: cclib is not installed correctly!" << endl;
 		return false;
 	}
 	if (!openedFile) {
-		cout << "Testing of cclib Failed!" << endl;
+		cout << "Testing of cclib failed!" << endl;
 		cout << "\tReason: Unable to open test file: " << unitTestFile << endl;
 		return false;
 	}
 	if (!readEnergy) {
-		cout << "Testing of cclib Failed!" << endl;
+		cout << "Testing of cclib failed!" << endl;
 		cout << "\tReason: Unable to read the energy from: " << unitTestFile << endl;
 		return false;
 	}
 	cout << "Energy: " << energy << " au " << endl;
 	if (obtainedGeometry) {
-		cout << "Testing of cclib Failed!" << endl;
+		cout << "Testing of cclib failed!" << endl;
 		cout << "\tReason: Read geometry when it wasn't supposed to have been read from: " << unitTestFile << endl;
 		return false;
 	}
@@ -71,24 +71,24 @@ bool ccLibReadTest()
 	cout << "Reading energy and geometry from file: " << unitTestFile.c_str() << endl;
 	Energy::readOutputFileWithCCLib(unitTestFile.c_str(), energy, &moleculeSet, cclibInstalled, openedFile, readEnergy, obtainedGeometry);
 	if (!openedFile) {
-		cout << "Testing of cclib Failed!" << endl;
+		cout << "Testing of cclib failed!" << endl;
 		cout << "\tReason: Unable to open test file: " << unitTestFile << endl;
 		return false;
 	}
 	if (!readEnergy) {
-		cout << "Testing of cclib Failed!" << endl;
+		cout << "Testing of cclib failed!" << endl;
 		cout << "\tReason: Unable to read the energy from: " << unitTestFile << endl;
 		return false;
 	}
 	cout << "Energy: " << moleculeSet.getEnergy() << " au " << endl;
 	if (!obtainedGeometry) {
-		cout << "Testing of cclib Failed!" << endl;
+		cout << "Testing of cclib failed!" << endl;
 		cout << "\tReason: Unable to read the geometry from: " << unitTestFile << endl;
 		return false;
 	}
 	moleculeSet.printToScreen();
 	if (moleculeSet.getIsTransitionState()) {
-		cout << "Testing of cclib Failed!" << endl;
+		cout << "Testing of cclib failed!" << endl;
 		cout << "\tReason: The structure in the output file is not a transition state: " << unitTestFile << endl;
 		return false;
 	}
@@ -97,32 +97,34 @@ bool ccLibReadTest()
 	cout << "Reading energy and geometry from file: " << unitTestFile2.c_str() << endl;
 	Energy::readOutputFileWithCCLib(unitTestFile2.c_str(), energy, &moleculeSet, cclibInstalled, openedFile, readEnergy, obtainedGeometry);
 	if (!openedFile) {
-		cout << "Testing of cclib Failed!" << endl;
+		cout << "Testing of cclib failed!" << endl;
 		cout << "\tReason: Unable to open test file: " << unitTestFile2 << endl;
 		return false;
 	}
 	if (!readEnergy) {
-		cout << "Testing of cclib Failed!" << endl;
+		cout << "Testing of cclib failed!" << endl;
 		cout << "\tReason: Unable to read the energy from: " << unitTestFile2 << endl;
 		return false;
 	}
 	cout << "Energy: " << moleculeSet.getEnergy() << " au " << endl;
 	if (!obtainedGeometry) {
-		cout << "Testing of cclib Failed!" << endl;
+		cout << "Testing of cclib failed!" << endl;
 		cout << "\tReason: Unable to read the geometry from: " << unitTestFile2 << endl;
 		return false;
 	}
 	moleculeSet.printToScreen();
 	if (!moleculeSet.getIsTransitionState()) {
-		cout << "Testing of cclib Failed!" << endl;
+		cout << "Testing of cclib failed!" << endl;
 		cout << "\tReason: The structure in the output file is a transition state: " << unitTestFile2 << endl;
 		return false;
 	}
 	cout << "Is a transition state: " << moleculeSet.getIsTransitionState() << endl;
 	
-	cout << "Testing of cclib Passed!" << endl;
+	cout << "Testing of cclib passed!" << endl;
 	return true;
 }
+
+
 
 int main(int argc, char *argv[])
 {
